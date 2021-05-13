ec8a2e48108adda9de565df7a55e348bdef1e549
NielzosFilms <nielzosfilms@gmail.com>
2021-05-13 12:26:29 +0200

    logging in dev

--- models/index.js
+++ models/index.js
@@ -21,32 +21,36 @@ let sequelize;
 // }
 
 sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASS,
-    { host: process.env.DB_HOST, dialect: process.env.DIALECT }
+	process.env.DB_NAME,
+	process.env.DB_USER,
+	process.env.DB_PASS,
+	{
+		host: process.env.DB_HOST,
+		dialect: process.env.DIALECT,
+		...(process.env.NODE_ENV === "production" && {logging: false}),
+	}
 );
 
 fs.readdirSync(__dirname)
-    .filter((file) => {
-        return (
-            file.indexOf(".") !== 0 &&
-            file !== basename &&
-            file.slice(-3) === ".js"
-        );
-    })
-    .forEach((file) => {
-        const model = require(path.join(__dirname, file))(
-            sequelize,
-            Sequelize.DataTypes
-        );
-        db[model.name] = model;
-    });
+	.filter((file) => {
+		return (
+			file.indexOf(".") !== 0 &&
+			file !== basename &&
+			file.slice(-3) === ".js"
+		);
+	})
+	.forEach((file) => {
+		const model = require(path.join(__dirname, file))(
+			sequelize,
+			Sequelize.DataTypes
+		);
+		db[model.name] = model;
+	});
 
 Object.keys(db).forEach((modelName) => {
-    if (db[modelName].associate) {
-        db[modelName].associate(db);
-    }
+	if (db[modelName].associate) {
+		db[modelName].associate(db);
+	}
 });
 
 db.sequelize = sequelize;
