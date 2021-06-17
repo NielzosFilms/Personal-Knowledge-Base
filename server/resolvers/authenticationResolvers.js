20dd727e0cce3b166655a780f2d3aecf6055cba2
NielzosFilms <nielzosfilms@gmail.com>
2021-06-17 20:46:44 +0200

    https enabled

--- server/resolvers/authenticationResolvers.js
+++ server/resolvers/authenticationResolvers.js
@@ -4,166 +4,166 @@ const crypto = require("crypto");
 const nodemailer = require("nodemailer");
 
 const transport = {
-    host: "smtp.gmail.com",
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-    },
+	host: "smtp.gmail.com",
+	auth: {
+		user: process.env.EMAIL_USER,
+		pass: process.env.EMAIL_PASS,
+	},
 };
 
 const transporter = nodemailer.createTransport(transport);
 
 transporter.verify((error, success) => {
-    if (error) {
-        console.log(error);
-    } else {
-        console.log("Nodemailer connected!");
-    }
+	if (error) {
+		console.log(error);
+	} else {
+		console.log("Nodemailer connected!");
+	}
 });
 
 const resolvers = {
-    Query: {
-        verifyEmailToken: async (root, { token }, { models }) => {
-            const emailToken = await models.EmailToken.findOne({
-                where: {
-                    token,
-                },
-            });
-            if (emailToken) {
-                return { success: true, email: emailToken.email };
-            }
-            throw new Error("The given token was not found.");
-        },
-        isAuthenticated: async (root, args, { loggedIn }) => {
-            return loggedIn;
-        },
-        getAuthenticatedUser: async (root, args, { loggedIn, user }) => {
-            if (!loggedIn) return null;
-            return user;
-        },
-    },
-    Mutation: {
-        login: async (root, { username, password }, { models, loggedIn }) => {
-            if (loggedIn) return { success: false, token: null };
-            const user = await models.User.findOne({
-                where: {
-                    name: username,
-                },
-            });
+	Query: {
+		verifyEmailToken: async (root, {token}, {models}) => {
+			const emailToken = await models.EmailToken.findOne({
+				where: {
+					token,
+				},
+			});
+			if (emailToken) {
+				return {success: true, email: emailToken.email};
+			}
+			throw new Error("The given token was not found.");
+		},
+		isAuthenticated: async (root, args, {loggedIn}) => {
+			return loggedIn;
+		},
+		getAuthenticatedUser: async (root, args, {loggedIn, user}) => {
+			if (!loggedIn) return null;
+			return user;
+		},
+	},
+	Mutation: {
+		login: async (root, {username, password}, {models, loggedIn}) => {
+			if (loggedIn) return {success: false, token: null};
+			const user = await models.User.findOne({
+				where: {
+					name: username,
+				},
+			});
 
-            if (user) {
-                if (passwordHash.verify(password, user.password)) {
-                    const token = crypto.randomBytes(64).toString("hex");
-                    const session = await models.Session.create({
-                        token,
-                        user_id: Number(user.id),
-                    });
-                    session.save();
-                    return { success: true, token };
-                }
-            }
-            throw new Error("Username and or password given was not correct.");
-        },
-        logout: async (root, args, { models, loggedIn, user, token }) => {
-            if (!loggedIn || !user || !token) return false;
-            await models.Session.destroy({
-                where: {
-                    token,
-                },
-            });
-            return true;
-        },
-        sendVerifyEmail: async (root, { email }, { models }) => {
-            const token = crypto.randomBytes(64).toString("hex");
-            const user = await models.User.findOne({
-                where: {
-                    email,
-                },
-            });
-            if (!user) {
-                const port =
-                    process.env.NODE_ENV === "production"
-                        ? process.env.SERVER_PORT
-                        : "3000";
-                return transporter
-                    .sendMail({
-                        from: "NielzosFilms Knowledge Base",
-                        to: email,
-                        subject:
-                            "Confirm email address to create your account!",
+			if (user) {
+				if (passwordHash.verify(password, user.password)) {
+					const token = crypto.randomBytes(64).toString("hex");
+					const session = await models.Session.create({
+						token,
+						user_id: Number(user.id),
+					});
+					session.save();
+					return {success: true, token};
+				}
+			}
+			throw new Error("Username and or password given was not correct.");
+		},
+		logout: async (root, args, {models, loggedIn, user, token}) => {
+			if (!loggedIn || !user || !token) return false;
+			await models.Session.destroy({
+				where: {
+					token,
+				},
+			});
+			return true;
+		},
+		sendVerifyEmail: async (root, {email}, {models}) => {
+			const token = crypto.randomBytes(64).toString("hex");
+			const user = await models.User.findOne({
+				where: {
+					email,
+				},
+			});
+			if (!user) {
+				const port =
+					process.env.NODE_ENV === "production"
+						? process.env.SERVER_PORT
+						: "3000";
+				return transporter
+					.sendMail({
+						from: "NielzosFilms Knowledge Base",
+						to: email,
+						subject:
+							"Confirm email address to create your account!",
 
-                        html: `<a href="http://${process.env.HOST}:${port}/create-user/token/${token}">Click here to create your account</a>`,
-                    })
-                    .then(async (res) => {
-                        await models.EmailToken.destroy({
-                            where: {
-                                email,
-                            },
-                        });
+						html: `<a href="https://${process.env.HOST}:${port}/create-user/token/${token}">Click here to create your account</a>`,
+					})
+					.then(async (res) => {
+						await models.EmailToken.destroy({
+							where: {
+								email,
+							},
+						});
 
-                        let expiration = new Date();
-                        expiration.setHours(expiration.getHours() + 1);
-                        const emailToken = await models.EmailToken.create({
-                            token,
-                            email,
-                            expiration,
-                        });
-                        await emailToken.save();
-                        return true;
-                    })
-                    .catch((error) => {
-                        console.log(error);
-                        throw new Error("The email was invalid.");
-                    });
-            } else {
-                throw new Error("A user was found with this email address.");
-            }
-        },
-        sendChangePasswordEmail: async (root, { email }, { models }) => {
-            const token = crypto.randomBytes(64).toString("hex");
-            if (!email) throw new Error("No email has been provided.");
-            const user = await models.User.findOne({
-                where: {
-                    email,
-                },
-            });
-            if (!user)
-                throw new Error("No user with this email has been found.");
-            const port =
-                process.env.NODE_ENV === "production"
-                    ? process.env.SERVER_PORT
-                    : "3000";
-            return transporter
-                .sendMail({
-                    from: "NielzosFilms Knowledge Base",
-                    to: email,
-                    subject: "Reset password / change username link.",
+						let expiration = new Date();
+						expiration.setHours(expiration.getHours() + 1);
+						const emailToken = await models.EmailToken.create({
+							token,
+							email,
+							expiration,
+						});
+						await emailToken.save();
+						return true;
+					})
+					.catch((error) => {
+						console.log(error);
+						throw new Error("The email was invalid.");
+					});
+			} else {
+				throw new Error("A user was found with this email address.");
+			}
+		},
+		sendChangePasswordEmail: async (root, {email}, {models}) => {
+			const token = crypto.randomBytes(64).toString("hex");
+			if (!email) throw new Error("No email has been provided.");
+			const user = await models.User.findOne({
+				where: {
+					email,
+				},
+			});
+			if (!user)
+				throw new Error("No user with this email has been found.");
+			const port =
+				process.env.NODE_ENV === "production"
+					? process.env.SERVER_PORT
+					: "3000";
+			return transporter
+				.sendMail({
+					from: "NielzosFilms Knowledge Base",
+					to: email,
+					subject: "Reset password / change username link.",
 
-                    html: `<a href="http://${process.env.HOST}:${port}/reset/${user.id}/token/${token}">Click here to reset your password / change username</a>`,
-                })
-                .then(async (res) => {
-                    await models.EmailToken.destroy({
-                        where: {
-                            email,
-                        },
-                    });
+					html: `<a href="https://${process.env.HOST}:${port}/reset/${user.id}/token/${token}">Click here to reset your password / change username</a>`,
+				})
+				.then(async (res) => {
+					await models.EmailToken.destroy({
+						where: {
+							email,
+						},
+					});
 
-                    let expiration = new Date();
-                    expiration.setHours(expiration.getHours() + 1);
-                    const emailToken = await models.EmailToken.create({
-                        token,
-                        email,
-                        expiration,
-                    });
-                    await emailToken.save();
-                    return true;
-                })
-                .catch((error) => {
-                    console.log(error);
-                    throw new Error("The email was invalid.");
-                });
-        },
-    },
+					let expiration = new Date();
+					expiration.setHours(expiration.getHours() + 1);
+					const emailToken = await models.EmailToken.create({
+						token,
+						email,
+						expiration,
+					});
+					await emailToken.save();
+					return true;
+				})
+				.catch((error) => {
+					console.log(error);
+					throw new Error("The email was invalid.");
+				});
+		},
+	},
 };
 
 module.exports = resolvers;
--- server/server.js
+++ server/server.js
@@ -9,95 +9,95 @@ dotenv.config();
 const http = require("http");
 const https = require("https");
 const privateKey = fs.readFileSync(
-    path.join(__dirname, "../sslcert", "server.key")
+	path.join(__dirname, "../sslcert", "server.key")
 );
 const certificate = fs.readFileSync(
-    path.join(__dirname, "../sslcert", "server.crt")
+	path.join(__dirname, "../sslcert", "server.crt")
 );
 
-const { ApolloServer, AuthenticationError } = require("apollo-server-express");
+const {ApolloServer, AuthenticationError} = require("apollo-server-express");
 const typeDefs = require(path.join(__dirname, "schema"));
 const resolvers = require(path.join(__dirname, "resolvers"));
 
 const sequelize = require(path.join(__dirname, "../models/index"));
 
 const credentials = {
-    key: privateKey,
-    cert: certificate,
+	key: privateKey,
+	cert: certificate,
 };
 
 const corsOptions = {
-    origin: `http://${process.env.HOST || "localhost"}:${
-        process.env.SERVER_PORT || "8080"
-    }`,
-    credentials: true,
+	origin: `https://${process.env.HOST || "localhost"}:${
+		process.env.SERVER_PORT || "8080"
+	}`,
+	credentials: true,
 };
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context: async ({ req }) => {
-        if (process.env.DISABLE_AUTHENTICATION === "1") {
-            return {
-                models: sequelize,
-                loggedIn: true,
-                user: {
-                    id: 1,
-                    name: "admin",
-                },
-                token: null,
-            };
-        }
-        const token = req.headers.authorization || "";
-        const session = await sequelize.Session.findOne({
-            where: {
-                token: token,
-            },
-        });
+	typeDefs,
+	resolvers,
+	context: async ({req}) => {
+		if (process.env.DISABLE_AUTHENTICATION === "1") {
+			return {
+				models: sequelize,
+				loggedIn: true,
+				user: {
+					id: 1,
+					name: "admin",
+				},
+				token: null,
+			};
+		}
+		const token = req.headers.authorization || "";
+		const session = await sequelize.Session.findOne({
+			where: {
+				token: token,
+			},
+		});
 
-        if (session) {
-            const user = await sequelize.User.findOne({
-                where: {
-                    id: session.user_id,
-                },
-            });
-            if (user) {
-                return {
-                    models: sequelize,
-                    loggedIn: true,
-                    user,
-                    token,
-                };
-            }
-        }
-        return {
-            models: sequelize,
-            loggedIn: false,
-            user: null,
-            token: null,
-        };
-    },
+		if (session) {
+			const user = await sequelize.User.findOne({
+				where: {
+					id: session.user_id,
+				},
+			});
+			if (user) {
+				return {
+					models: sequelize,
+					loggedIn: true,
+					user,
+					token,
+				};
+			}
+		}
+		return {
+			models: sequelize,
+			loggedIn: false,
+			user: null,
+			token: null,
+		};
+	},
 });
 
 console.log(process.env.NODE_ENV);
 
 server.applyMiddleware({
-    app,
-    path: "/graphql",
-    ...(process.env.NODE_ENV === "production" && { cors: corsOptions }),
+	app,
+	path: "/graphql",
+	...(process.env.NODE_ENV === "production" && {cors: corsOptions}),
 });
 
 app.use(express.static(path.join(__dirname, "../build")));
 
 app.use(function (req, res) {
-    res.sendFile(path.join(__dirname, "../build", "index.html"));
+	res.sendFile(path.join(__dirname, "../build", "index.html"));
 });
 
-app.listen(process.env.SERVER_PORT || 8080);
+// app.listen(process.env.SERVER_PORT || 8080);
 
 // const httpServer = http.createServer(app);
 // httpServer.listen(8080);
-// const httpsServer = https.createServer(credentials, app);
-// httpsServer.listen(8080);
+const httpsServer = https.createServer(credentials, app);
+httpsServer.listen(8080);
 
 console.log(`Listening on port ${process.env.SERVER_PORT || 8080}`);
